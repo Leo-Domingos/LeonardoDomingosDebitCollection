027a3f904d6907f0ed2bd33e83c7c662f1e02cdd
Leonardo Domingos <leonardosilvadls@gmail.com>
2023-08-01 08:40:15 -0300

    fluxo até a previsão

--- helper_functions.py
+++ helper_functions.py
@@ -28,14 +28,14 @@ def plot_all_cat_freq(df, figsize=(15, 15)):
         if len(df[column].unique()) > 5:
             # Select the 5 most frequent categories
             top_5_categories = df[column].value_counts().nlargest(5).index
-            plot_cat_freq(df[df[column].isin(top_5_categories)][column].reset_index(drop = True), title=column, ax=axes[row_idx, col_idx])
+            plot_cat_freq(df[df[column].isin(top_5_categories)][column], title=column, ax=axes[row_idx, col_idx])
         else:
             plot_cat_freq(df[column], title=column, ax=axes[row_idx, col_idx])
 
     plt.tight_layout()
     plt.show()
 
-def remove_outliers(df):
+def remove_outliers(df, bound_const = 1.5):
     df_out = df.copy()
     
     # Calculate the first quartile (Q1) and third quartile (Q3)
@@ -46,10 +46,28 @@ def remove_outliers(df):
     IQR = Q3 - Q1
         
     # Calculate the lower and upper outlier boundaries
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
+    lower_bound = Q1 - bound_const * IQR
+    upper_bound = Q3 + bound_const * IQR
 
     for col in df_out.columns:
         df_out = df_out[(df_out[col] >= lower_bound[col]) & (df_out[col] <= upper_bound[col])]
     
     return list(df_out.index)
+
+def create_stacked_bar_plot(ax, df, col_x, col_y, title):
+    counts = df.groupby([col_x, col_y]).size().unstack(fill_value=0)
+    x_labels = df[col_x].unique()
+    sorted_counts = counts.loc[x_labels]
+    
+    ax = sorted_counts.plot(kind='bar', stacked=True, ax=ax)
+    ax.set_title(title)
+    ax.set_xlabel(col_x)
+    ax.set_ylabel("Contagem")
+
+    # Custom legend with all categories
+    handles, labels = ax.get_legend_handles_labels()
+    labels = [f"{value:.0f}" for label in labels for value in sorted_counts.columns]
+    ax.legend(handles, labels, title=col_y)
+
+    for container in ax.containers:
+        ax.bar_label(container, label_type='center')
